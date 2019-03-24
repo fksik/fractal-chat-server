13fdb39fa8bb4f1b31f3b6f83119500344aaf34e
saikumar <saikumar.kusuma@gmail.com>
2019-03-24 12:18:40 +0530

    Updated models Conversaion

--- model/Conversation.ts
+++ model/Conversation.ts
@@ -1,9 +1,26 @@
-import { Model, Ref } from '@tsed/mongoose';
+import { Model, Ref, Schema } from '@tsed/mongoose';
 
 import { Account } from './Account';
+import { User } from './User';
+import { PropertyType } from '@tsed/common';
+
+@Schema()
+export class Participant {
+	@Ref(User)
+	user: Ref<User>
+	addedAt: number;
+	@Ref(User)
+	addedBy: Ref<User>;
+}
 
 @Model()
 export class Conversation {
 	public _id: string;
+	public isPrivate: boolean;
+	public url: string;
+	@Ref(User)
+	public createdBy: Ref<User>;
+	@PropertyType(Participant)
+	public participants: Participant[];
 	@Ref(Account) public account: Ref<Account>;
 }
--- services/ChatService.ts
+++ services/ChatService.ts
@@ -1,16 +1,23 @@
-import {IO, Nsp, Socket, SocketService} from '@tsed/socketio';
-import {Namespace, Server, Socket as SocketIO} from 'socket.io';
-import {$log} from 'ts-log-debug';
+import { IO, Nsp, Socket, SocketService, Input } from '@tsed/socketio';
+import { Namespace, Server, Socket as SocketIO } from 'socket.io';
+import { $log } from 'ts-log-debug';
 
 @SocketService('/chat.io')
 export class ChatService {
-  @Nsp private nsp: Namespace;
+	@Nsp private nsp: Namespace;
 
-  constructor(@IO private io: Server) {
-  }
+	constructor(@IO private io: Server) {}
 
-  public $onConnection(@Socket socket: SocketIO) {
-    $log.info('connected');
+	public $onConnection(@Socket socket: SocketIO) {
+		$log.info('connected');
+	}
+	public $onDisconnect(@Socket socket: SocketIO) {}
+
+	@Input('message')
+	public message() {}
+
+	@Input('userTyping')
+	public userTyping() {
+    console.log('typing');
   }
-  public $onDisconnect(@Socket socket: SocketIO) {}
 }
